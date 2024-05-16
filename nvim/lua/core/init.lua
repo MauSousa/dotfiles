be251e09fc5899b9cd1c451bdf273762cf9be5da
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-16 11:36:27 -0600

    Add function to highlight yanked text

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -139,6 +139,15 @@ vim.api.nvim_create_autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
   end,
 })
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
+  end,
+})
+
 -------------------------------------- commands ------------------------------------------
 local new_cmd = vim.api.nvim_create_user_command
 
