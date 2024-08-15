1ee0038774409f0c10cd6ec6fc5cda15aae2ea08
Augusto Sousa <lagrange.tesla@gmail.com>
2024-08-15 17:01:24 -0600

    Add wezterm files

--- files.sh
+++ files.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cp -r ~/.config/nvim/ ~/.bashrc ~/.bash_aliases ~/.profile ~/.bash_functions ~/.config/starship ~/terminal.json ~/.config/kitty ~/.config/fastfetch/ ~/.config/.dotfiles/
+cp -r ~/.config/nvim/ ~/.bashrc ~/.bash_aliases ~/.profile ~/.bash_functions ~/.config/starship ~/terminal.json ~/.config/kitty ~/.config/fastfetch/ ~/.wezterm.lua ~/.config/.dotfiles/
 rm -rf ~/.config/.dotfiles/nvim/.git ~/.config/.dotfiles/nvim/.github
