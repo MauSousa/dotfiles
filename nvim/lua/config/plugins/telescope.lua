f60adaeda08bf285b63ea955fab1fa116c0a6969
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-01-08 18:30:32 -0600

    Add keymap for hidden files

--- nvim/lua/config/plugins/telescope.lua
+++ nvim/lua/config/plugins/telescope.lua
@@ -9,11 +9,13 @@ return {
     },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set('n', '<leader>en', function()
+      vim.keymap.set('n', '<space>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<space>fa', "<cmd>Telescope find_files follow=true no_ignore=true hidden=true <CR>",
+        { desc = 'Telescope find all files' })
+      vim.keymap.set('n', '<space>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<space>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<space>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<space>en', function()
           require('telescope.builtin').find_files {
             cwd = vim.fn.stdpath("config")
           }
